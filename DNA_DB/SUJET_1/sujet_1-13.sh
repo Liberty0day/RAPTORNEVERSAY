838713ee2ffd7d827574f0c70ce303b8a9efb918
Liberty0day <liberty188888@gmail.com>
2022-02-18 21:45:54 +0100

    up sujet_1

---
+++ DNA_DB/SUJET_1/sujet_1-13.sh
@@ -0,0 +1,12 @@
+echo '
+--] NETWORK SNIFFING 13
+
+OPERATOR   === KALI         IP === 132
+
+OPERATOR    ==> TASK ==> wireshark
+OPERATOR    ==> TASK ==> wireshark ==> File ==> Export Objects ==> HTTP
+OPERATOR    ==> TASK ==> wireshark ==> search kali image in jpg 
+
+'
+read
+clear
